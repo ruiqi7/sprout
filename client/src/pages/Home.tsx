377ec5e993be6c9ca316d3d86a18f86f5bcc57c5
Ruiqi Tin <tinruiqi2002@gmail.com>
2023-01-04 14:22:11 +0800

    Add Home.css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,21 @@
-import NavBar from "../components/NavBar";
+import Blooming from '../assets/Blooming';
+import NavBar from '../components/NavBar';
+import './Home.css';
 
 const Home: React.FC = () => {
     return (
-        <NavBar />
+        <div className="home">
+            <NavBar />
+            <div className="home_contents">
+                <span className="home_description">Sprout isn't just any discussion forum. It's a space for GROWTH.</span>
+                <div className="home_slogan">
+                    <span className="home_share">Share</span>
+                    <span className="home_learn">Learn</span>
+                    <span className="home_grow">Grow</span>
+                </div>
+                <Blooming />
+            </div>
+        </div>
     );
 }
 
